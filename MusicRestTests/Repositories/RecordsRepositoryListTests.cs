0f215bef260f8481de420e51bf7a04d3a56bef8e
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-11-09 14:12:26 +0100

    repo getalltest

--- MusicRestTests/Repositories/RecordsRepositoryListTests.cs
+++ MusicRestTests/Repositories/RecordsRepositoryListTests.cs
@@ -8,13 +8,20 @@ using System.Threading.Tasks;
 
 namespace MusicRest.Tests
 {
+
+
     [TestClass()]
     public class RecordsRepositoryListTests
     {
+        RecordsRepositoryList recordsRepositoryList = new RecordsRepositoryList(); // liste kopi
+
         [TestMethod()]
-        public void RecordsRepositoryListTest()
+        public void RecordsRepositoryGetAllFromListTest()
         {
-            Assert.Fail();
+            var Result = recordsRepositoryList.GetAll();
+            Assert.AreEqual(Result.Count(), 10);
+            Assert.AreEqual(Result.ElementAt(0).Title, "The Dark Side of the Moon");
+
         }
     }
 }
