33caf8bf396043fd5459f66d61423ceac670bacb
Jonathan-Ramus <ramus7000@gmail.com>
2023-11-09 14:31:46 +0100

    Added Controller

---
+++ MusicRest/Controllers/RecordsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicRest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecordsController : Controller
+    {
+        private RecordsRepositoryList _recordsRepository;
+
+        public RecordsController(RecordsRepositoryList recordsRepository)
+        {
+            _recordsRepository = recordsRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult<Record> Get()
+        {
+            IEnumerable<Record> result = _recordsRepository.GetAll();
+            if (result.Any()) { return Ok(result); }
+            return NoContent();
+        }
+    }
+}
