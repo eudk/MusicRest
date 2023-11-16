bf4b5ff54e2bdef0dea62d997abcc207359330a7
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-11-16 12:46:59 +0100

    controller getall

--- MusicRest/Controllers/RecordsController.cs
+++ MusicRest/Controllers/RecordsController.cs
@@ -16,11 +16,18 @@ namespace MusicRest.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public ActionResult<Record> Get()
+        public ActionResult<Record> Get([FromQuery] string? title = null, [FromQuery] string? artist = null, [FromQuery] int? duration = null, [FromQuery] int? publicationYear = null)
         {
-            IEnumerable<Record> result = _recordsRepository.GetAll();
+            IEnumerable<Record> result = _recordsRepository.Get(title: title ?? "", artist:artist??"", duration:duration?? 0 ,publicationYear:publicationYear?? 0 );
+
             if (result.Any()) { return Ok(result); }
             return NoContent();
+            
         }
+
+
     }
+
+
+
 }
